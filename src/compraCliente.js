19d1a1a29785c84ccde382316cdc1fd4296b31ad
Vitor <vitor@debian.Vitor>
2023-08-16 19:16:18 -0300

    feat: 'somente  erro de escrita no arquivo teste de compra

--- src/compraCliente.js
+++ src/compraCliente.js
@@ -2,8 +2,9 @@ import { CaixaDaLanchonete } from "./caixa-da-lanchonete.js";
 
 const caixa = new CaixaDaLanchonete();
 
-const metodoDePagamento = ('dinheiro');
-const itensSelecionados = ['queijo,1ls'];
+const metodoDePagamento = 'dinheiro';
+const itensSelecionados = ['queijo,1'];
+
 const resultado = caixa.calcularValorDaCompra(metodoDePagamento, itensSelecionados);
 
 console.log(resultado);
